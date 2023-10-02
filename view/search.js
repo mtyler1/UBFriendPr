92663f2e5e1100407d59c23c022e48180fcb97db
mtyler1 <121874021+mtyler1@users.noreply.github.com>
2023-10-02 13:13:48 -0400

    Update search.js

--- view/search.js
+++ view/search.js
@@ -30,3 +30,39 @@ function search(input, template) {
   // Treat the input as a search query
   return template.replace("%s", encodeURIComponent(input));
 }
+
+
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    alert("Failed to register service worker.");
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
